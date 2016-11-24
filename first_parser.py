a20b25297c3eff1c99239a67645e404f23716d2e
psfriso <pedrosfriso@gmail.com>
2016-11-24 10:32:28 +0000

    first working version

--- first_parser.py
+++ first_parser.py
@@ -1,7 +1,7 @@
 import sys
 import re
 
-_test_lim = 10000
+_test_lim = 1E10
 
 mp_pattern = re.compile(r'^MP:[0-9]*')
 
@@ -14,31 +14,33 @@ class novel_gene( object ):
         self.alleles_impc = set()
 
     def find_alleles_in_mgi(self, l ):
-        if l is None:
-            pass
-        else:
+        if l:
             for elem in l:
                 self.alleles_mgi.add( elem )
 
     def find_alleles_in_impc(self, l ):
-        if l is None:
-            pass
-        else:
+        if l :
             for elem in l:
                 self.alleles_impc.add( elem )
 
-    def new_alleles(self):
+    def new_alleles( self ):
         # alleles in impc that match (or are absent) in mgi
         not_even_in_mgi = self.alleles_impc - self.alleles_mgi
 
         intersection = self.alleles_impc & self.alleles_mgi
 
-        return not_even_in_mgi | intersection
+        return not_even_in_mgi | intersection # union of two sets
 
-    def are_all_allele_new(self):
+    def are_all_allele_new( self ):
         new = self.new_alleles()
         ans = len( self.alleles_mgi - new ) == 0
         return ans
+
+    def known_alleles( self ):
+        return  self.alleles_mgi - self.alleles_impc
+
+    def has_known_alleles( self ):
+        return len( self.known_alleles() ) > 0
 
 
 
@@ -74,16 +76,20 @@ with open(sys.argv[2] , 'r' ) as f:
     f.readline()
     for l in f:
         if c > _test_lim: break
-        line_elem = l.rstrip().split(",")
+        line_elem = l.rstrip().split("\t")
         c += 1
 #        print( line_elem )
         mps = [ elem for elem in line_elem if is_mp_term(elem) ]
-        if line_elem[1] in impc:
-            impc[ line_elem[1] ][0].append( line_elem[6] )
-            impc[ line_elem[1] ][1].append( line_elem[0] )
-            impc[ line_elem[1] ][2].append(  ",".join(mps) )
-        else:
-            impc[ line_elem[1] ] = [  [line_elem[6]] , [line_elem[0]] , [ ",".join(mps) ] ]
+        try:
+            if line_elem[1] in impc:
+                impc[ line_elem[1] ][0].append( line_elem[6] )
+                impc[ line_elem[1] ][1].append( line_elem[0] )
+                impc[ line_elem[1] ][2].append(  ",".join(mps) )
+            else:
+                impc[ line_elem[1] ] = [  [line_elem[6]] , [line_elem[0]] , [ ",".join(mps) ] ]
+        except IndexError:
+            print( "\tSkipping\t",line_elem )
+
 f.close()
 
 
@@ -113,23 +119,21 @@ for gene in gene_list:
         gene_list[ gene ].find_alleles_in_impc( None )
 
 
-
+c = 0
 for gene in gene_list:
-
-    # for al in  gene_list[ gene ].alleles_mgi:
-    #     print( "with allele mgi", al)
-    # for al in  gene_list[ gene ].alleles_impc:
-    #     print( "with allele impc", al)
-
+#    print( gene_list[gene].name )
     if gene_list[ gene ].are_all_allele_new():
-        print()
-        print( gene_list[gene].name , gene_list[gene].marker_id )
-    #    print( "All alleles are new")
-        for al in gene_list[ gene ].new_alleles():
-            print( "new allele", al)
+        c += 1
+        print( gene_list[gene].name , gene_list[gene].marker_id , end = '\t')
+        print( "new", [ al for al in gene_list[ gene ].new_alleles() ] )
 
+    if gene_list[ gene ].has_known_alleles():
+        print( gene_list[gene].name , gene_list[gene].marker_id , end = '\t')
+        print( "known" , [ al for al in gene_list[ gene ].known_alleles() ] )
 
-print( gene_list["Ssr2"].alleles_impc )
-print( gene_list["Ssr2"].alleles_mgi )
+print( "Number of new genes ", c , " total ")
+
+# print( gene_list["Ssr2"].alleles_impc )
+# print( gene_list["Ssr2"].alleles_mgi )
 
 sys.exit("")
